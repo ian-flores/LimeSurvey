6b4d41b97da38a41a0766108ae055f07f7152841
Gordon55M <52419582+Gordon55M@users.noreply.github.com>
2019-07-01 14:33:34 -0500

    Update limesurveyinstall.sh

--- limesurveyinstall.sh
+++ limesurveyinstall.sh
@@ -20,12 +20,11 @@ sudo apt-get install nginx zip unzip pwgen -y
 tput setaf 2; echo "Setting Up NGINX..."
 sleep 2;
 tput sgr0
-cd /etc/nginx/sites-available/
-
-sudo wget -qO "$DOMAIN" https://raw.githubusercontent.com/Gordon55M/LimeSurvey/master/sites-available/example.com.conf
-sudo sed -i -e "s/example.com/$DOMAIN/" "$DOMAIN"
-sudo sed -i -e "s/www.example.com/www.$DOMAIN/" "$DOMAIN"
-sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
+# cd /etc/nginx/sites-available/
+# sudo wget -qO "$DOMAIN" https://raw.githubusercontent.com/Gordon55M/LimeSurvey/master/sites-available/example.com.conf
+# sudo sed -i -e "s/example.com/$DOMAIN/" "$DOMAIN"
+# sudo sed -i -e "s/www.example.com/www.$DOMAIN/" "$DOMAIN"
+# sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
 sudo mkdir /etc/nginx/limelemp
 cd /etc/nginx/limelemp
 sudo wget -q https://raw.githubusercontent.com/Gordon55M/LimeSurvey/master/limelemp/general.conf
@@ -42,11 +41,11 @@ sudo certbot --nginx
 
 
 sudo mkdir /etc/nginx/ssl
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
-cd /etc/nginx/
-sudo mv nginx.conf nginx.conf.backup
-sudo wget -qO nginx.conf https://raw.githubusercontent.com/Gordon55M/LimeSurvey/master/nginx.conf
+#cd /etc/nginx/
+#sudo mv nginx.conf nginx.conf.backup
+#sudo wget -qO nginx.conf https://raw.githubusercontent.com/Gordon55M/LimeSurvey/master/nginx.conf
 sudo mkdir -p /var/www/"$DOMAIN"/public
 cd /var/www/"$DOMAIN/public"
 cd ~
